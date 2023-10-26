f108734d7da6a2a36059f87e691ad39ba3012e1b
Enzo Bordogna <e-bordogna@onu.edu>
2023-10-26 10:42:00 -0400

    normal

--- P9-1.1/Source.cpp
+++ P9-1.1/Source.cpp
@@ -7,3 +7,43 @@
  */
 
 
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int randNum(void);
+void printNormal(const int* a);
+void printEvenIn(const int* a);
+void printEvenVal(const int* a);
+void printReverse(const int* a);
+void printFirstLast(const int* a);
+
+int main() {
+	srand(time(NULL));
+	int randArray[10] = { randNum(), randNum(), randNum(), randNum(), randNum(), randNum(), randNum(), randNum(), randNum(), randNum() };
+	printNormal(randArray);
+	printEvenIn(randArray);
+	printEvenVal(randArray);
+	printReverse(randArray);
+	printFirstLast(randArray);
+
+	cout << "Press any key to continue . . .";
+	return 0;
+}
+
+int randNum(void) {
+	int num;
+	num = rand() % 9000 + 999;
+	return num;
+}
+void printNormal(const int* a) {
+	cout << "The random integers : ";
+	const int* p = a;
+	for (int i = 0; i < 10; i++) {
+		cout << *p << " ";
+		p++;
+	}
+	cout << endl;
+}
